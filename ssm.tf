2e0db913cfea45e6bef07ea4bb730203a7c92c51
Eugene Romancenko <eugene.romancenko@and.digital>
2021-06-16 15:42:15 +0000

    Resolve PRI-386 "Feature/ api gw policy json"

--- ssm.tf
+++ ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "api_gateway_id" {
-  name        = "/${var.environment}/${var.application_service}/api-gateway-id"
+  name        = "/${var.application_service}/api-gateway-id"
   description = "The ID of the api gateway"
   type        = "String"
   value       = aws_api_gateway_rest_api.private.id
@@ -7,7 +7,7 @@ resource "aws_ssm_parameter" "api_gateway_id" {
 }
 
 resource "aws_ssm_parameter" "root_resource_id" {
-  name        = "/${var.environment}/${var.application_service}/api-root-resource-id"
+  name        = "/${var.application_service}/api-root-resource-id"
   description = "The ID of the root resource of the api gateway"
   type        = "String"
   value       = aws_api_gateway_rest_api.private.root_resource_id
@@ -15,7 +15,7 @@ resource "aws_ssm_parameter" "root_resource_id" {
 }
 
 resource "aws_ssm_parameter" "api_policy_json" {
-  name        = "/${var.environment}/${var.application_service}/api-policy-json"
+  name        = "/${var.application_service}/api-policy-json"
   description = "Policy to allow from vpc endpoint to api gateway"
   type        = "SecureString"
   value       = data.aws_iam_policy_document.allow_from_vpc_endpoint_to_gateway.json
